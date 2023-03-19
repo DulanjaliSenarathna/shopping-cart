27b51cb677a0848bae945771c3021fdb01a5da56
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-20 00:48:29 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div >
+    <Cart/>
+    <RouterView/>
+  </div>
+</template>
+
+<script setup> 
+    import {RouterView} from "vue-router";
+    import Cart from "./components/Cart.vue";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="grid grid-cols-2 gap-4">
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+      hi
+    </div>
+    <div class="bg-white rounded-lg shadow-lg overflow-hidden">
+     hi
+    </div>
+  </div>
+  </template>
+ 
+<script setup>
+
+</script>
+
