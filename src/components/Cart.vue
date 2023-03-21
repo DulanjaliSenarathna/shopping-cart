77350222d89c764d93588a6be2b74baded03079f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-21 22:57:23 +0530

    Design completed

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,36 +1,40 @@
 <template>
   <div class=" flex">
-    <div class="  w-3/5 max-w-md mt-20 ml-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-  <div class="md:flex">
+    <div class="  w-3/5 max-w-md mt-4 ml-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <p class="mt-2 ml-5"> <strong>Cart</strong> </p>
+      
+  <div class="md:flex" v-for="item in products" :key="item.id">
+   
     <div class="md:flex-shrink-0">
-      <img class="h-20 w-20 object-cover rounded-lg ml-5 mt-5 mb-5" src="https://i.ibb.co/rGf1nwB/tshirt.jpg" alt="Card Image">
+      
+      <img class="h-20 w-20 object-cover rounded-lg ml-5 mt-5 mb-5" :src="item.imageUrl" :alt="item.title">
     </div>
     <div class="ml-3 mt-4">
       <div class="flex justify-between">
-        <p class="text-zinc-500 flex space-x-2 text-sm">Relaxed Fit T-Shirt</p>
-        <div class="text-black text-sm font-bold">$99.00</div>
+        <p class="text-zinc-500 flex space-x-2 text-sm">{{ item.title }}</p>
+        <div class="text-black text-sm font-bold">{{ item.price }}</div>
       </div>
       <div class=" flex">
-        <label class="text-zinc-400 text-xs mt-1"> $12.99</label>
-        <p class="text-[#32CD32] ml-4"> <strong>In Stock</strong> </p>
+        <label class="text-zinc-400 text-xs mt-1"> {{ item.price }}</label>
+        <p class="text-[#32CD32] ml-4 mt-1 text-xs"> <strong>In Stock</strong> </p>
       </div>
       <div class="flex">
         <div class="mt-3">
-        <div class="text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20 ">
-                <p class="text-xs mr-4 ml-3">XL</p>
-                <div class="mr-2 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-up-fill cursor-pointer" viewBox="0 0 16 16">
+        <div class="shadow-sm text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20 ">
+                <p class="text-xs mr-4 ml-3">{{ item.size }}</p>
+                <div class="mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-up-fill cursor-pointer text-xs" viewBox="0 0 16 16">
                   <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-down-fill cursor-pointer" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-down-fill cursor-pointer text-xs" viewBox="0 0 16 16">
                 <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                 </svg>
                 </div>
             </div>
       </div>
       <div class="mt-3 ml-10">
-        <div class="text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20 ">
-                <p class="text-xs mr-4 ml-3">Blue</p>
+        <div class="shadow-sm text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20 ">
+                <p class="text-xs mr-4 ml-3">{{ item.color }}</p>
                 <div class="mr-2 ">
                   <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-up-fill cursor-pointer" viewBox="0 0 16 16">
                   <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
@@ -41,6 +45,7 @@
                 </div>
             </div>
       </div>
+      
       <div class=" flex rounded ml-10 mt-3 h-7">
             <div class="text-zinc-500 w-8 flex items-center justify-center cursor-pointer rounded-l-md border-t border-l border-b">
                 -
@@ -50,43 +55,82 @@
         </div>
 
         <div class="flex ml-12 justify-center">
-      <svg  style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill mt-3" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill="#8c8c97"></path> </svg>
+      <svg  style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill mt-3 cursor-pointer" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill="#8c8c97"></path> </svg>
       <p class="text-xs ml-2 mt-3">Save</p>
     </div>
     <div class="flex ml-12 justify-center">
-      <svg style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill mt-3" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" fill="#8c8c97"></path> </svg>
+      <svg style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill mt-3 cursor-pointer" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" fill="#8c8c97"></path> </svg>
       <p class="text-xs ml-2 mt-3 ">Delete</p>
      </div>
       </div>
-      
+      <hr class="mt-5"/>
     </div>
+    
   </div>
+  
 </div>
-<div class="max-w-md mt-20 ml-5 mr-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl w-2/5">
+
+<div class="max-w-md w-80 mt-4 ml-5 mr-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl w-2/5">
+  <div class="divide-y divide-dashed mt-5 mr-5 ml-5 mb-3">
   <div class="md:flex">
-    <div class="md:flex-shrink-0">
-      <img class="h-48 w-full object-cover md:w-48" src="product-image.jpg" alt="Product image">
-    </div>
-    <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Product name</div>
-      <div class="block mt-1 text-lg leading-tight font-medium text-black">Product description</div>
+    <div>
+      <p class="text-zinc-500 flex space-x-2 text-sm">Delivery</p>
       <div class="mt-2">
-        <div class="text-gray-500 text-sm">Price:</div>
-        <div class="text-black text-lg font-bold">$99.00</div>
-      </div>
-      <div class="mt-4">
-        <label class="block text-gray-500 font-bold">Quantity:</label>
-        <input class="w-20 h-10 px-3 py-2 text-gray-700 border rounded-md focus:outline-none focus:shadow-outline" type="number" value="1">
-      </div>
-      <div class="mt-6">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          Add to cart
-        </button>
+        <div class= " items-center h-8 flex text-zinc-500  flex  items-start justify-start  rounded-md border w-40  bg-[#EEEEEE] ">
+          	<button class=" h-7 rounded-md border text-sm w-10 bg-white shadow-md ">Free</button>
+            <button class="text-sm ml-3 ">Express: $9.99</button>
+        </div>
+        <p class="text-zinc-400 text-xs mt-2 mb-5">Delivery date: june 24, 2022 </p> 
       </div>
     </div>
+  </div>
+  <div>
+        <div class= " items-center h-8 mt-5 flex text-zinc-500  flex justify-between rounded-md border w-70 ">
+            <button class="text-sm ml-3 ">Promocode</button>
+          	<button class=" h-8 rounded-md border text-sm w-12 bg-white shadow-sm "> <strong> Apply</strong></button>
+        </div>
+        <p class="text-zinc-400 text-xs mt-2 mb-5">20% off discount</p> 
+  </div>
+  <div>
+    <div class="flex justify-between">
+      <p class="text-zinc-500 flex space-x-2 text-sm mt-5">Subtotal</p>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-5">$80.96</p>
+    </div>
+    <div class="flex justify-between">
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-3">Discount</p>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-3">(20%) - $16.19</p>
+    </div>
+    <div class="flex justify-between">
+      <div class="flex">
+        <p class="text-zinc-500 flex space-x-2 text-xs mt-3">Delivery</p>
+        <i class="fa-solid fa-circle-info fa-xs mt-5 ml-1" style="color: #a6adb9;"></i>
+      </div>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-3">$0.00</p>
+    </div>
+    <div class="flex justify-between mb-5">
+      <div class="flex">
+        <p class="text-zinc-500 flex space-x-2 text-xs mt-3">Tax</p>
+        <i class="fa-solid fa-circle-info fa-xs mt-5 ml-1" style="color: #a6adb9;"></i>
+      </div>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-3">+$14.00</p>
+    </div>
+  </div>
+  <div class="flex justify-between ">
+      <p class="text-zinc-500 flex space-x-2 text-sm mt-5">Total</p>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-5">$78.76</p>   
+  </div>      
+</div>
+ <div >
+  <button class=" h-10 items-center w-72 ml-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Proceed to checkout
+    </button>   
+    <button class="mt-2 h-10 w-72 ml-5  hover:bg-blue-500 border border-blue-500 hover:text-white shadow-lg  text-blue-700 font-bold py-2 px-4 rounded">
+          Continue shopping
+    </button> 
+ </div>
+      
   </div>
 </div>
-  </div>
   
 
 
@@ -94,10 +138,12 @@
   </template>
  
 <script>
+import products from '../mock/products.json';
  export default{
    name:"ProductList",
    data(){
     return{
+      products,
       
     }
    }
