bdc5407676d707c992ac57528b2cc2014a39fd85
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-22 09:19:36 +0530

    Completed

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,13 +1,15 @@
 <template >
   <div class=" flex">
-    <div ref="div1" class="  w-3/5 max-w-md mt-4 ml-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl md:flex-row sm:flex-row md:flex-shrink-0 sm:flex-shrink-0">
-      <p class="mt-2 ml-5"> <strong>Cart</strong> </p>
+    <div ref="div1" 
+    class="  w-3/5 max-w-md mt-4 ml-20 bg-white rounded-xl shadow-md 
+    overflow-hidden md:max-w-2xl md:flex-row sm:flex-row md:flex-shrink-0 sm:flex-shrink-0">
+    <p class="mt-2 ml-5"> <strong>Cart</strong> </p>
       
   <div class="md:flex" v-for="item in items" :key="item.id">
    
     <div class="md:flex-shrink-0">
       
-      <img class="h-20 w-20 object-cover rounded-lg ml-5 mt-5 mb-5" :src="item.imageUrl" :alt="item.title">
+    <img class="h-20 w-20 object-cover rounded-lg ml-5 mt-5 mb-5" :src="item.imageUrl" :alt="item.title">
     </div>
     <div class="ml-3 mt-4">
       <div class="flex justify-between">
@@ -23,11 +25,15 @@
         <div class="shadow-sm text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20  ">
                 <p class="text-xs mr-4 ml-3 ">{{ item.size }}</p>
                 <div class="mr-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-up-fill cursor-pointer text-xs" viewBox="0 0 16 16">
-                  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" 
+                  fill="currentColor" class="bi bi-caret-up-fill cursor-pointer text-xs" viewBox="0 0 16 16">
+                  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1
+                   1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-down-fill cursor-pointer text-xs" viewBox="0 0 16 16">
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" 
+                fill="currentColor" class="bi bi-caret-down-fill cursor-pointer text-xs" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 
+                1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                 </svg>
                 </div>
             </div>
@@ -36,33 +42,48 @@
         <div class="shadow-sm md:flex-row sm:flex-row text-zinc-500 w-15 flex items-center justify-center  rounded-md border w-20 ">
                 <p class="text-xs mr-4 ml-3">{{ item.color }}</p>
                 <div class="mr-2 ">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-up-fill cursor-pointer" viewBox="0 0 16 16">
-                  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor"
+                   class="bi bi-caret-up-fill cursor-pointer" viewBox="0 0 16 16">
+                  <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1
+                   1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-caret-down-fill cursor-pointer" viewBox="0 0 16 16">
-                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" 
+                class="bi bi-caret-down-fill cursor-pointer" viewBox="0 0 16 16">
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 
+                1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
                 </svg>
                 </div>
             </div>
       </div>
       
       <div class=" flex rounded ml-10 mt-3 h-7 md:flex-shrink-0">
-            <div class="text-zinc-500 w-8 flex items-center justify-center cursor-pointer rounded-l-md border-t border-l border-b" @click="decreaseQuantity(item)">
+            <div class="text-zinc-500 w-8 flex items-center justify-center 
+            cursor-pointer rounded-l-md border-t border-l border-b" @click="decreaseQuantity(item)">
                 -
             </div>
             <div class="w-8 flex items-center  text-xs justify-center border-t border-b">{{item.quantity }}</div>
-            <div class="text-zinc-500 w-8 flex items-center justify-center cursor-pointer rounded-r-md  text-s border-b border-t border-r " @click="increaseQuantity(item)">
+            <div class="text-zinc-500 w-8 flex items-center justify-center cursor-pointer
+             rounded-r-md  text-s border-b border-t border-r " @click="increaseQuantity(item)">
               +
             </div>
             
         </div>
 
         <div class="flex ml-12 justify-center md:flex-row sm:flex-row md:flex-shrink-0">
-      <svg  style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill mt-3 cursor-pointer" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill="#8c8c97"></path> </svg>
+      <svg  style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" 
+      height="16" fill="currentColor" class="bi bi-heart-fill mt-3 cursor-pointer" 
+      viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 
+      4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" fill="#8c8c97"></path> </svg>
       <p class="text-xs ml-2 mt-3">Save</p>
     </div>
     <div class="flex ml-12 justify-center md:flex-row sm:flex-row md:flex-shrink-0">
-      <svg style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill mt-3 cursor-pointer" viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" fill="#8c8c97"></path> </svg>
+      <svg style="color: rgb(140, 140, 151);" xmlns="http://www.w3.org/2000/svg" width="16"
+       height="16" fill="currentColor" class="bi bi-trash-fill mt-3 cursor-pointer"
+        viewBox="0 0 16 16"> <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2
+         2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 
+         1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 
+         0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3
+          .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" fill="#8c8c97"></path> </svg>
       <p class="text-xs ml-2 mt-3 ">Delete</p>
      </div>
       </div>
@@ -72,15 +93,17 @@
   </div>
    
 </div>
-
-<div ref="div2" class="max-w-md w-80 mt-4 ml-5 mr-20 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl w-2/5">
+<div class="max-w-md w-80 mt-4 ml-5 mr-20 bg-white rounded-xl shadow-md
+ overflow-hidden md:max-w-2xl w-2/5">
   <div class="divide-y divide-dashed mt-5 mr-5 ml-5 mb-0">
   <div class="md:flex">
     <div>
       <p class="text-zinc-500 flex space-x-2 text-sm">Delivery</p>
       <div class="mt-2" >
-        <div  class= " items-center h-8 flex text-zinc-500  flex  items-start justify-start  rounded-md border w-40  bg-[#EEEEEE] ">
-          	<button class=" h-7 rounded-md border text-sm w-10 bg-white shadow-md ">Free</button>
+        <div  class= " items-center h-8 flex text-zinc-500  flex 
+         items-start justify-start  rounded-md border w-40  bg-[#EEEEEE] ">
+          	<button class=" h-7 rounded-md border text-sm w-10 bg-white shadow-md ">
+              Free</button>
             <button class="text-sm ml-3 ">Express: $9.99</button>
         </div>
         <p class="text-zinc-400 text-xs mt-2 mb-5">Delivery date: june 24, 2022 </p> 
@@ -88,16 +111,18 @@
     </div>
   </div>
   <div>
-        <div class= " items-center h-8 mt-5 flex text-zinc-500  flex justify-between rounded-md border w-70 ">
+        <div class= " items-center h-8 mt-5 flex text-zinc-500  flex justify-between
+         rounded-md border w-70 ">
             <button class="text-sm ml-3 ">Promocode</button>
-          	<button class=" h-8 rounded-md border text-sm w-12 bg-white shadow-sm "> <strong> Apply</strong></button>
+          	<button class=" h-8 rounded-md border text-sm w-12 bg-white shadow-sm "> 
+              <strong> Apply</strong></button>
         </div>
         <p class="text-zinc-400 text-xs mt-2 mb-5">20% off discount</p> 
   </div>
   <div>
     <div class="flex justify-between" >
       <p class="text-zinc-500 flex space-x-2 text-sm mt-5">Subtotal</p>
-      <p class="text-zinc-500 flex space-x-2 text-xs mt-5">{{ }}</p>
+      <p class="text-zinc-500 flex space-x-2 text-xs mt-5">$0.00</p>
     </div>
     <div class="flex justify-between">
       <p class="text-zinc-500 flex space-x-2 text-xs mt-3">Discount</p>
@@ -123,10 +148,14 @@
       <p class="text-zinc-500 flex space-x-2 text-xs mt-5">$78.76</p>   
   </div>      
 </div>
-            <button class=" p-2 items-center  mt-3 m-auto ml-20 sm:mr-16  bg-blue-500 hover:bg-blue-700 text-white rounded-md  ">Proceed to checkout</button>
-            <button class=" p-2 pr-4 items-center  mt-2 m-auto ml-20 sm:mr-16  border border-blue-500 hover:bg-blue-100 text-blue-700 rounded-md  ">Continue shopping</button>
+  <button class=" p-2 items-center  mt-3 m-auto ml-20 sm:mr-16  bg-blue-500
+   hover:bg-blue-700 text-white rounded-md  ">Proceed to checkout</button>
+  <button class=" p-2 pr-4 items-center  mt-2 m-auto ml-20 sm:mr-16  
+  border border-blue-500 hover:bg-blue-100 text-blue-700 rounded-md  ">
+  Continue shopping</button>
       
   </div>
+
 </div>
  
   </template>
@@ -139,7 +168,7 @@
       items: [
     {
         "íd":1,
-        "title":"T shirt",
+        "title":"Relaxed Fit T-shirt",
         "price":12.99,
         "status":"In Stock",
         "size":"XL",
@@ -149,23 +178,24 @@
     },
     {
         "íd":2,
-        "title":"Cap",
+        "title":"Nylon Sports Cap",
         "price":15.36,
         "status":"In Stock",
         "size":"XL",
         "color":"blue",
         "quantity":3,
-        "imageUrl":"https://i.ibb.co/rGf1nwB/tshirt.jpg"
-    },
+        "imageUrl":"https://i.ibb.co/9GKp4sq/hmgoepprod-2-removebg-preview.jpg"
+       },
     {
         "íd":3,
-        "title":"Cap",
+        "title":"Sneakers",
         "price":12.99,
         "status":"In Stock",
         "size":"XL",
         "color":"blue",
         "quantity":4,
-        "imageUrl":"https://i.ibb.co/rGf1nwB/tshirt.jpg"
+        "imageUrl":"https://i.ibb.co/5n9Vh1Y/sneakers.jpg"
+        
     },
     {
         "íd":3,
@@ -175,8 +205,8 @@
         "size":"XL",
         "color":"Blue",
         "quantity":8,
-        "imageUrl":"https://i.ibb.co/rGf1nwB/tshirt.jpg"
-    }
+        "imageUrl":"https://i.ibb.co/nzJhq0F/images-removebg-preview.jpg"
+         }
 ]
     }
    },
